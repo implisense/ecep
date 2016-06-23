ce0eadbda11517e41107afc270d3b6248c02cc92
Hannes Korte <email@hkorte.com>
2016-06-23 19:20:10 +0200

    Replace library csvreader by commons-csv

--- api/src/main/java/com/implisense/ecep/api/format/CompaniesHouseFormatParser.java
+++ api/src/main/java/com/implisense/ecep/api/format/CompaniesHouseFormatParser.java
@@ -1,98 +1,260 @@
 package com.implisense.ecep.api.format;
 
+import com.implisense.ecep.api.util.SicTitleProvider;
+import com.implisense.ecep.index.model.Address;
+import com.implisense.ecep.index.model.Company;
+import com.implisense.ecep.index.model.PreviousName;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.apache.commons.lang3.time.DateParser;
+import org.apache.commons.lang3.time.FastDateFormat;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.text.ParseException;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import static com.google.common.base.Strings.isNullOrEmpty;
+
+@Singleton
 public class CompaniesHouseFormatParser {
 
-    private int NAME = -1;
-    private int NUMBER = -1;
-    private int ADDR_CAREOF = -1;
-    private int ADDR_POBOX = -1;
-    private int ADDR_LINE1 = -1;
-    private int ADDR_LINE2 = -1;
-    private int ADDR_TOWN = -1;
-    private int ADDR_COUNTY = -1;
-    private int ADDR_COUNTRY = -1;
-    private int ADDR_POSTCODE = -1;
-    private int CATEGORY = -1;
-    private int STATUS = -1;
-    private int COUNTRYOFORIGIN = -1;
-    private int DISSOLUTIONDATE = -1;
-    private int INCORPORATIONDATE = -1;
-    private int URI = -1;
-    private int SICCODESSTART = -1;
-    private int SICCODESEND = -1;
-    private int PREVIOUSNAMESSTART = -1;
-    private int PREVIOUSNAMESEND = -1;
+    private SicTitleProvider sicTitleProvider;
 
-    public CompaniesHouseFormatParser(String[] header) {
-        this.parseHeader(header);
+    @Inject
+    public CompaniesHouseFormatParser(SicTitleProvider sicTitleProvider) {
+        this.sicTitleProvider = sicTitleProvider;
     }
 
-    private void parseHeader(String[] header) {
-        for (int i = 0; i < header.length; i++) {
-            switch (header[i]) {
-                case "CompanyName":
-                    this.NAME = i;
-                    break;
-                case "CompanyNumber":
-                    this.NUMBER = i;
-                    break;
-                case "RegAddress.CareOf":
-                    this.ADDR_CAREOF = i;
-                    break;
-                case "RegAddress.POBox":
-                    this.ADDR_POBOX = i;
-                    break;
-                case "RegAddress.AddressLine1":
-                    this.ADDR_LINE1 = i;
-                    break;
-                case "RegAddress.AddressLine2":
-                    this.ADDR_LINE2 = i;
-                    break;
-                case "RegAddress.PostTown":
-                    this.ADDR_TOWN = i;
-                    break;
-                case "RegAddress.County":
-                    this.ADDR_COUNTY = i;
-                    break;
-                case "RegAddress.Country":
-                    this.ADDR_COUNTRY = i;
-                    break;
-                case "RegAddress.PostCode":
-                    this.ADDR_POSTCODE = i;
-                    break;
-                case "CompanyCategory":
-                    this.CATEGORY = i;
-                    break;
-                case "CompanyStatus":
-                    this.STATUS = i;
-                    break;
-                case "CountryOfOrigin":
-                    this.COUNTRYOFORIGIN = i;
-                    break;
-                case "DissolutionDate":
-                    this.DISSOLUTIONDATE = i;
-                    break;
-                case "IncorporationDate":
-                    this.INCORPORATIONDATE = i;
-                    break;
-                case "URI":
-                    this.URI = i;
-                    break;
+    public Iterable<Company> iterateCompanies(byte[] input, Charset charset) {
+        this.sicTitleProvider.putAll(extractSicCodeTitleMap(input, charset));
+        final Iterator<CSVRecord> records = buildReader(input, charset);
+        final ColumnIndices idx = parseHeader(records.next());
+        return new Iterable<Company>() {
+            @Override
+            public Iterator<Company> iterator() {
+                return new Iterator<Company>() {
+
+                    @Override
+                    public boolean hasNext() {
+                        return records.hasNext();
+                    }
+
+                    @Override
+                    public Company next() {
+                        Company company = parseLine(records.next(), idx);
+                        return company;
+                    }
+                };
             }
-            if (header[i].startsWith("SICCode")) {
-                if (SICCODESSTART == -1) {
-                    SICCODESSTART = i;
-                }
-            } else if(SICCODESSTART != -1 && SICCODESEND == -1) {
-                SICCODESEND = i;
+        };
+    }
+
+    private static class ColumnIndices {
+        private int NAME = -1;
+        private int NUMBER = -1;
+        private int ADDR_CAREOF = -1;
+        private int ADDR_POBOX = -1;
+        private int ADDR_LINE1 = -1;
+        private int ADDR_LINE2 = -1;
+        private int ADDR_TOWN = -1;
+        private int ADDR_COUNTY = -1;
+        private int ADDR_COUNTRY = -1;
+        private int ADDR_POSTCODE = -1;
+        private int CATEGORY = -1;
+        private int STATUS = -1;
+        private int COUNTRYOFORIGIN = -1;
+        private int DISSOLUTIONDATE = -1;
+        private int INCORPORATIONDATE = -1;
+        private int URI = -1;
+        private int SICCODESSTART = -1;
+        private int SICCODESEND = -1;
+        private int PREVIOUSNAMESSTART = -1;
+        private int PREVIOUSNAMESEND = -1;
+    }
+
+    private static Map<String, String> extractSicCodeTitleMap(byte[] input, Charset charset) {
+        Iterator<CSVRecord> records = buildReader(input, charset);
+        final ColumnIndices idx = parseHeader(records.next());
+        Map<String, String> sicCodeTitleMap = new HashMap<>();
+        while (records.hasNext()) {
+            CSVRecord record = records.next();
+            addSicCodeTitlesToMap(record, idx, sicCodeTitleMap);
+        }
+        return sicCodeTitleMap;
+    }
+
+    private static Iterator<CSVRecord> buildReader(byte[] input, Charset charset) {
+        InputStream is;
+        // make it a ZipInputStream if it is readable as such
+        try {
+            ZipInputStream zis = new ZipInputStream(new ByteArrayInputStream(input));
+            ZipEntry entry = zis.getNextEntry();
+            if (entry == null) {
+                is = new ByteArrayInputStream(input);
+            } else {
+                is = zis;
             }
-            if (header[i].startsWith("PreviousName")) {
-                if (PREVIOUSNAMESSTART == -1) {
-                    PREVIOUSNAMESSTART = i;
-                }
-            } else if(PREVIOUSNAMESSTART != -1 && PREVIOUSNAMESEND == -1) {
-                PREVIOUSNAMESEND = i;
+        } catch (IOException e) {
+            is = new ByteArrayInputStream(input);
+        }
+        try {
+            return CSVFormat.RFC4180.parse(new InputStreamReader(is, charset)).iterator();
+        } catch (IOException e) {
+            throw new CompaniesHouseParsingException("Invalid CSV file format!", e);
+        }
+    }
+
+    private static final Pattern SIC_CODE_TITLE_PATTERN = Pattern.compile("^(\\d+) - (.+)$");
+
+    private static void addSicCodeTitlesToMap(CSVRecord record, ColumnIndices idx, Map<String, String> sicCodeTitleMap) {
+        for (int i = idx.SICCODESSTART; i < idx.SICCODESEND; i++) {
+            Matcher m = SIC_CODE_TITLE_PATTERN.matcher(record.get(i));
+            if (m.find()) {
+                sicCodeTitleMap.put(m.group(1), m.group(2));
             }
         }
+    }
+
+    private static Company parseLine(CSVRecord line, ColumnIndices idx) {
+        Company company = new Company();
+        company.setName(line.get(idx.NAME));
+        company.setId(line.get(idx.NUMBER));
+        Address address = new Address();
+        address.setCareOf(line.get(idx.ADDR_CAREOF));
+        address.setPoBox(line.get(idx.ADDR_POBOX));
+        address.setLine1(line.get(idx.ADDR_LINE1));
+        address.setLine2(line.get(idx.ADDR_LINE2));
+        address.setTown(line.get(idx.ADDR_TOWN));
+        address.setCounty(line.get(idx.ADDR_COUNTY));
+        address.setCountry(line.get(idx.ADDR_COUNTRY));
+        address.setPostCode(line.get(idx.ADDR_POSTCODE));
+        company.setAddress(address);
+        company.setCategory(line.get(idx.CATEGORY));
+        company.setStatus(line.get(idx.STATUS));
+        company.setCountryOfOrigin(line.get(idx.COUNTRYOFORIGIN));
+        company.setIncorporationDate(parseDate(line.get(idx.INCORPORATIONDATE)));
+        company.setDissolutionDate(parseDate(line.get(idx.DISSOLUTIONDATE)));
+        company.setUri(line.get(idx.URI));
+        List<String> sicCodes = new ArrayList<>(idx.SICCODESEND - idx.SICCODESSTART);
+        for (int i = idx.SICCODESSTART; i < idx.SICCODESEND; i++) {
+            Matcher m = SIC_CODE_TITLE_PATTERN.matcher(line.get(i));
+            if (m.find()) {
+                sicCodes.add(m.group(1));
+            }
+        }
+        company.setSicCodes(sicCodes);
+        List<PreviousName> previousNames = new ArrayList<>(idx.PREVIOUSNAMESEND - idx.PREVIOUSNAMESSTART);
+        for (int i = idx.PREVIOUSNAMESSTART; i < idx.PREVIOUSNAMESEND; i += 2) {
+            Date changeDate = parseDate(line.get(i));
+            String name = line.get(i + 1).trim();
+            if (changeDate != null && !"".equals(name)) {
+                previousNames.add(new PreviousName(changeDate, name));
+            }
+        }
+        company.setPreviousNames(previousNames);
+        return company;
+    }
+
+    private static final String DATE_PATTERN = "dd/MM/yyyy";
+    private static final DateParser DATE_PARSER = FastDateFormat.getInstance(DATE_PATTERN,
+            TimeZone.getTimeZone("Europe/London"), Locale.ENGLISH);
+
+    private static Date parseDate(String dateString) {
+        try {
+            String trimmed = dateString.trim();
+            if (isNullOrEmpty(trimmed)) {
+                return null;
+            } else {
+                return DATE_PARSER.parse(dateString);
+            }
+        } catch (ParseException e) {
+            throw new CompaniesHouseParsingException("Unexpected date format: \"" + dateString
+                    + "\" - expected pattern: \"" + DATE_PATTERN + "\"", e);
+        }
+    }
+
+    private static ColumnIndices parseHeader(CSVRecord header) {
+        ColumnIndices idx = new ColumnIndices();
+        for (int i = 0; i < header.size(); i++) {
+            String trimmed = header.get(i).trim();
+            switch (trimmed) {
+                case "CompanyName":
+                    idx.NAME = i;
+                    break;
+                case "CompanyNumber":
+                    idx.NUMBER = i;
+                    break;
+                case "RegAddress.CareOf":
+                    idx.ADDR_CAREOF = i;
+                    break;
+                case "RegAddress.POBox":
+                    idx.ADDR_POBOX = i;
+                    break;
+                case "RegAddress.AddressLine1":
+                    idx.ADDR_LINE1 = i;
+                    break;
+                case "RegAddress.AddressLine2":
+                    idx.ADDR_LINE2 = i;
+                    break;
+                case "RegAddress.PostTown":
+                    idx.ADDR_TOWN = i;
+                    break;
+                case "RegAddress.County":
+                    idx.ADDR_COUNTY = i;
+                    break;
+                case "RegAddress.Country":
+                    idx.ADDR_COUNTRY = i;
+                    break;
+                case "RegAddress.PostCode":
+                    idx.ADDR_POSTCODE = i;
+                    break;
+                case "CompanyCategory":
+                    idx.CATEGORY = i;
+                    break;
+                case "CompanyStatus":
+                    idx.STATUS = i;
+                    break;
+                case "CountryOfOrigin":
+                    idx.COUNTRYOFORIGIN = i;
+                    break;
+                case "DissolutionDate":
+                    idx.DISSOLUTIONDATE = i;
+                    break;
+                case "IncorporationDate":
+                    idx.INCORPORATIONDATE = i;
+                    break;
+                case "URI":
+                    idx.URI = i;
+                    break;
+            }
+            if (trimmed.startsWith("SICCode")) {
+                if (idx.SICCODESSTART == -1) {
+                    idx.SICCODESSTART = i;
+                }
+            } else if (idx.SICCODESSTART != -1 && idx.SICCODESEND == -1) {
+                idx.SICCODESEND = i;
+            }
+            if (trimmed.startsWith("PreviousName")) {
+                if (idx.PREVIOUSNAMESSTART == -1) {
+                    idx.PREVIOUSNAMESSTART = i;
+                }
+            } else if (idx.PREVIOUSNAMESSTART != -1 && idx.PREVIOUSNAMESEND == -1) {
+                idx.PREVIOUSNAMESEND = i;
+            }
+        }
+        if (idx.PREVIOUSNAMESEND == -1) {
+            idx.PREVIOUSNAMESEND = header.size();
+        }
+        return idx;
     }
 }
---
+++ api/src/main/java/com/implisense/ecep/api/format/CompaniesHouseParsingException.java
@@ -0,0 +1,18 @@
+package com.implisense.ecep.api.format;
+
+public class CompaniesHouseParsingException extends RuntimeException {
+    public CompaniesHouseParsingException() {
+    }
+
+    public CompaniesHouseParsingException(String message) {
+        super(message);
+    }
+
+    public CompaniesHouseParsingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public CompaniesHouseParsingException(Throwable cause) {
+        super(cause);
+    }
+}
