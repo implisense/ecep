6500fc054a82d491e1892bf814ad0aa0b3801458
Hannes Korte <email@hkorte.com>
2016-09-06 19:19:29 +0200

    Change content import logic to ES update API

--- api/src/main/java/com/implisense/ecep/api/resources/DataResource.java
+++ api/src/main/java/com/implisense/ecep/api/resources/DataResource.java
@@ -8,8 +8,8 @@ import com.implisense.ecep.api.model.UploadResponseStatus;
 import com.implisense.ecep.api.model.exceptions.SimpleWebException;
 import com.implisense.ecep.api.model.exceptions.WebError;
 import com.implisense.ecep.index.EcepIndex;
+import com.implisense.ecep.index.EcepIndexException;
 import com.implisense.ecep.index.model.Company;
-import com.implisense.ecep.index.model.ContentField;
 import org.glassfish.jersey.media.multipart.FormDataParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,12 +20,10 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.zip.GZIPInputStream;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
@@ -66,18 +64,18 @@ public class DataResource {
             List<Company> bulk = new ArrayList<>(BULK_SIZE);
             for (Company company : companiesHouseFormatParser.iterateCompanies(input, Charsets.US_ASCII)) {
                 bulk.add(company);
-                if(bulk.size() == BULK_SIZE) {
+                if (bulk.size() == BULK_SIZE) {
                     numImported += bulk.size();
                     ecepIndex.putCompanies(bulk);
                     bulk = new ArrayList<>(BULK_SIZE);
                 }
             }
-            if(!bulk.isEmpty()) {
+            if (!bulk.isEmpty()) {
                 numImported += bulk.size();
                 ecepIndex.putCompanies(bulk);
             }
             return new UploadResponse(UploadResponseStatus.OK, "Imported " + numImported + " companies.");
-        } catch(CompaniesHouseParsingException e) {
+        } catch (CompaniesHouseParsingException e) {
             throw new SimpleWebException(WebError.INVALID_FILE_FORMAT, e.getMessage());
         }
     }
@@ -91,66 +89,42 @@ public class DataResource {
     }
 
     private UploadResponse importContent(byte[] input) {
-        final int BULK_SIZE = 100;
-        final BufferedReader reader = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(input), UTF_8));
-        final List<String> ids = new ArrayList<>(BULK_SIZE);
-        final List<String> texts = new ArrayList<>(BULK_SIZE);
-        final List<Company> bulk = new ArrayList<>(BULK_SIZE);
+        final int BULK_SIZE = 500;
+        final List<String> lines = new ArrayList<>(BULK_SIZE);
         try {
+            final BufferedReader reader = buildReader(input);
             int numImported = 0;
             String line;
-            while((line = reader.readLine())!=null) {
-                int firstTag = line.indexOf("\t");
-                ids.add(line.substring(0, firstTag));
-                texts.add(line.substring(firstTag + 1));
-                if(ids.size() == BULK_SIZE) {
-                    List<Company> companies = ecepIndex.getCompanies(ids);
-                    for (int i = 0; i < BULK_SIZE; i++) {
-                        Company company = companies.get(i);
-                        if(company == null) {
-                            LOGGER.warn(String.format("No company found for id %s\n", ids.get(i)));
-                            continue;
-                        }
-                        if(!ids.get(i).equals(company.getId())) {
-                            throw new RuntimeException("Wrong order of company list!");
-                        }
-                        if(company.getContent() == null) {
-                            company.setContent(new ContentField());
-                        }
-                        company.getContent().setGeneral(texts.get(i));
-                        bulk.add(company);
-                    }
-                    ecepIndex.putCompanies(bulk);
-                    numImported += bulk.size();
-                    ids.clear();
-                    texts.clear();
-                    bulk.clear();
-                    LOGGER.info(String.format("%5d imported", numImported));
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
+                if (lines.size() == BULK_SIZE) {
+                    ecepIndex.putUrlsWithContent(lines.stream().map(l -> l.split("\t", -1)).toArray(String[][]::new));
+                    numImported += lines.size();
+                    lines.clear();
                 }
             }
-            if(!ids.isEmpty()) {
-                List<Company> companies = ecepIndex.getCompanies(ids);
-                for (int i = 0; i < companies.size(); i++) {
-                    Company company = companies.get(i);
-                    if(!ids.get(i).equals(company.getId())) {
-                        throw new RuntimeException("Wrong order of company list!");
-                    }
-                    if(company.getContent() == null) {
-                        company.setContent(new ContentField());
-                    }
-                    company.getContent().setGeneral(texts.get(i));
-                    bulk.add(company);
-                }
-                ecepIndex.putCompanies(bulk);
-                numImported += bulk.size();
-                ids.clear();
-                texts.clear();
-                bulk.clear();
-                LOGGER.info(String.format("%5d imported\n", numImported));
+            if (!lines.isEmpty()) {
+                ecepIndex.putUrlsWithContent(lines.stream().map(l -> l.split("\t", -1)).toArray(String[][]::new));
+                numImported += lines.size();
+                lines.clear();
             }
+            ecepIndex.refresh();
             return new UploadResponse(UploadResponseStatus.OK, "Imported " + numImported + " companies.");
-        } catch(CompaniesHouseParsingException | IOException e) {
+        } catch (EcepIndexException | IOException e) {
             throw new SimpleWebException(WebError.INVALID_FILE_FORMAT, e.getMessage());
         }
+    }
+
+    private BufferedReader buildReader(byte[] input) throws IOException {
+        InputStream inputStream = new ByteArrayInputStream(input);
+        if (isGzipContent(input)) {
+            inputStream = new GZIPInputStream(inputStream);
+        }
+        return new BufferedReader(new InputStreamReader(inputStream, UTF_8));
+    }
+
+    private boolean isGzipContent(byte[] input) {
+        int head = ((int) input[0] & 0xff) | ((input[1] << 8) & 0xff00);
+        return (GZIPInputStream.GZIP_MAGIC == head);
     }
 }
